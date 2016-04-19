263a4d73c27ec5096e658b77f191fbede6aa0688
Tibor Kranjcec <tibor@floatingpoint.io>
2016-04-19 13:57:38 +0200

    Modify landing page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,46 @@
-console.log('\'Allo \'Allo!');
+(function ($) {
+  "use strict";  
+  function navMobileCollapse() {
+    // avoid having both mobile navs opened at the same time
+    $('#collapsingMobileUser').on('show.bs.collapse', function () {
+      $('#collapsingNavbar').removeClass('in');
+      $('[data-target="#collapsingNavbar"]').attr('aria-expanded', 'false');
+    });
+    $('#collapsingNavbar').on('show.bs.collapse', function () {
+      $('#collapsingMobileUser').removeClass('in');
+      $('[data-target="#collapsingMobileUser"]').attr('aria-expanded', 'false');
+    });
+    // dark navbar
+    $('#collapsingMobileUserInverse').on('show.bs.collapse', function () {
+      $('#collapsingNavbarInverse').removeClass('in');
+      $('[data-target="#collapsingNavbarInverse"]').attr('aria-expanded', 'false');
+    });
+    $('#collapsingNavbarInverse').on('show.bs.collapse', function () {
+      $('#collapsingMobileUserInverse').removeClass('in');
+      $('[data-target="#collapsingMobileUserInverse"]').attr('aria-expanded', 'false');
+    });
+  }
+
+  function navSearch() {
+    // hide first nav items when search is opened
+    $('.nav-dropdown-search').on('show.bs.dropdown', function () {
+      $(this).siblings().not('.navbar-nav .dropdown').addClass('sr-only');
+    })
+    // cursor focus
+    $('.nav-dropdown-search').on('shown.bs.dropdown', function () {
+      $('.navbar-search-input').focus();
+    });
+    // show all nav items when search is closed
+    $('.nav-dropdown-search').on('hide.bs.dropdown', function () {
+      $(this).siblings().removeClass('sr-only');
+    });
+  }
+
+  function init() {
+    navMobileCollapse();
+    navSearch();
+  }
+
+  init();
+
+})(jQuery);
