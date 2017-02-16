3e4e5bc07465bf19582665fa47d0fc25dcbd23bb
Mirko Cerovac <mirko.cerovac@gmail.com>
2017-02-16 17:25:38 +0100

    Navbar and footer updates

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -1,5 +1,5 @@
 $ = require("jquery");
-
+jQuery = $;
 $( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
 
   var $target = $( event.currentTarget );
@@ -12,3 +12,26 @@ $( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
   return false;
 
 });
+
+(function($) {
+    "use strict";
+
+    var $navbar = $("#navbar"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+        $navbar.css('height', '4.5rem')
+    var $l_select = $('#localization-select')  
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos + height) {
+            $navbar.css('height', '2.5rem');
+            $l_select.css('height', '2.5rem');
+        } else if (scrollTop <= y_pos) {
+            $navbar.css('height', '4.5rem');
+            $l_select.css('height', '4.5rem');
+        }
+    });
+
+})(jQuery, undefined);
