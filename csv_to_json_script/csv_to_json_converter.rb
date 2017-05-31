8441a4e3d7a03892af23b4cd8e6d17966f69292d
Marko Požega <markopozega@floatingpoint.io>
2017-05-31 16:35:30 +0200

    refactor csv_to_json script

--- csv_to_json_script/csv_to_json_converter.rb
+++ csv_to_json_script/csv_to_json_converter.rb
@@ -1,10 +1,16 @@
 require 'csv'
 require 'json'
+require 'digest'
 
-final_hash = {}
+pabat_all = {}
+countries_parks = []
+country_order = []
+parks_indexed = []
+
 type_dict = {'Importance of value' => 'Exi',
              'Economic importance of value' => 'Eco',
-             'Exi' => 'Exi', 'Eco' => 'Eco'}
+             'Exi' => 'Exi',
+             'Eco' => 'Eco'}
 
 country_dict = {'Bosnia and Herzegovina' => 'BIH',
                 'Montenegro' => 'MNE',
@@ -26,15 +32,31 @@ country_dict = {'Bosnia and Herzegovina' => 'BIH',
 File.open('pa-bat.csv', 'r').each_with_index do |line, index|
   next if index == 0
   sv = line.split(';')
-  country = country_dict[sv[0]]
+  country = sv[0]
+  country_code = country_dict[country]
   pa = sv[1]
+  pa_code = Digest::MD5.hexdigest pa
   question = sv[4]
   type = type_dict[sv[5]]
   group = sv[7]
   value = sv[8].to_i
   potential = sv[10].to_i
 
-  ((((final_hash[country] ||= {})[pa] ||= {})[question] ||= {})[group] ||= {})[type] ||= {value: value, potential: potential}
+  ((((pabat_all[country_code] ||= {})[pa_code] ||= {})[question] ||= {})[group] ||= {})[type] ||= {value: value, potential: potential}
+
+  if country_order.include? country_code
+    if !parks_indexed.include? pa_code
+      countries_parks[country_order.index(country_code)][:protected_areas] << {code: pa_code, name: {en: pa, hr: pa}}
+      parks_indexed << pa_code
+    end
+  else
+    country_order << country_code
+    countries_parks << {name: {en: country, hr: country} ,code: country_code ,protected_areas: [{code: pa_code, name: {en: pa, hr: pa}}]}
+    parks_indexed << pa_code
+  end
+
 end
 
-File.open('../assets/static/pabat-all.json', 'w') { |file| file.write(final_hash.to_json) }
+File.open('../assets/static/pabat-all.json', 'w') { |file| file.write(pabat_all.to_json) }
+File.open('../assets/static/countries-parks.json', 'w') { |file| file.write(countries_parks.to_json) }
+
